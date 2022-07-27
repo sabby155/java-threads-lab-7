ae4ee8240264a8318eaf97c21ec5ee8d8764f08c
sabby155 <nebula155@gmail.com>
2022-07-27 13:59:23 -0400

    thread 7 complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,26 @@
 import java.util.Scanner;
-import java.util.concurrent.Callable;
+import java.util.concurrent.*;
 
 public class Main {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        Future<Integer> futureInteger = executor.submit(getCallableInteger());
+        System.out.println(futureInteger.get());
+        executor.shutdown();
+    }
     public static Callable<Integer> getCallableInteger() {
         // your code here
+        Scanner scanner = new Scanner(System.in);
+
+        Callable<Integer> callable = () -> {
+            System.out.println(Thread.currentThread().getName()+ " is responsible for this call");
+            System.out.println("Enter a number");
+            int num = scanner.nextInt();
+            return num;
+        };
+        return callable;
     }
+
+
 }
